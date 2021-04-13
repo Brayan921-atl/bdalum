b7776bdeda6348f6bc6491b045179e0cd7556799
Brayan921-atl <82236568+Brayan921-atl@users.noreply.github.com>
2021-04-12 19:46:23 -0500

    Update seguridad.js

--- js/seguridad.js
+++ js/seguridad.js
@@ -44,7 +44,7 @@ export async function
         usuario.email);
     for (const rol of roles) {
       if (rolIds.has(rol)) {
-        return false;
+        return true;
       }
     }
     alert("No autorizado.");
